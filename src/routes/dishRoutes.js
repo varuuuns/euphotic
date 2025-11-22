3ae54f6a80d6263c798eac1ee8651eb3d7841f5e
VARUN S <varuuun.s@gmail.com>
2025-11-22 11:29:27 +0530

    created dishroutes

---
+++ src/routes/dishRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import Dish from "../models/dish";
+
+const dishRouter = express.Router();
+
+dishRouter.get("/", async (req, res) => {
+    try {
+        const dishes = await Dish.find();
+        res.json({Dishes:dishes});
+    }
+    catch (err) {
+        console.log(`error from routes/dishRoutes.js: ${err}`);
+    }
+})
+
+dishRouter.put("/:id/toggle", async (req, res) => {
+    const dish = await Dish.findById(req.params.id);
+    if (!dish) return res.status(400).json({ msg: "invalid dish" });
+
+    dish.findByIdAndUpdate(!dish.isPublished);
+    io.emit("dish status updated", dish);
+
+    res.json({ Dish: dish });
+})
+
+return dishRouter;
